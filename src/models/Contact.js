b6c20954549fcd1c2bbefb76b5cfd2ec924cefe5
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-21 20:24:53 +0100

    Implemented contact model

---
+++ src/models/Contact.js
@@ -0,0 +1,11 @@
+export default class Contact{
+    constructor(contact = null){
+        console.log(contact)
+        if(contact == null) return
+        this.id = contact.id
+        this.name = contact.name
+        this.cellphone = (contact.cellphone) ? contact.cellphone : null
+        this.email = (contact.email) ? contact.email : null
+        this.address = (contact.address) ? contact.address : null
+    }
+}
