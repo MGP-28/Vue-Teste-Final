72f1cd7a848cfb6b1888720d3277b0fb961c06c8
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-21 20:26:03 +0100

    Created all related views

---
+++ src/views/Contacts.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="content-container">
+        <div class="toolbar">
+            <button class="btn btn-primary border" @click="createTask">Adicionar</button>
+        </div>
+        <ContentContainer>
+            <ContactCard v-for="(contact, idx) in contacts" :contact="contact" :key="idx" />
+        </ContentContainer>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia'
+import { useContactsStore } from '../stores/Contacts'
+import ContentContainer from '../components/ContentContainer.vue';
+import ContactCard from '../components/ContactCard.vue';
+    export default {
+    name: "ContactsView",
+    computed:{
+        ...mapState(useContactsStore, ['contacts'])
+    },
+    methods: {
+        createTask(){
+            //
+        },
+    },
+    components: { ContentContainer, ContactCard }
+}
+</script>
+
+<style scoped>
+.toolbar{
+    width: 100%;
+    display: flex;
+    justify-content: end;
+}
+</style>
